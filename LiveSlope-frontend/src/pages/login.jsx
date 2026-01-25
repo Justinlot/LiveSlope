a12a2a558d1b9e3e68acbea4eefcf071b2e68629
Justin Lotwin <161705340+Justinlot@users.noreply.github.com>
2026-01-25 17:03:04 +0100

    add design for login and register page

--- LiveSlope-frontend/src/pages/login.jsx
+++ LiveSlope-frontend/src/pages/login.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import '../styles/login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='container'>
+        <img src='/background.jpg' alt='background' className='background-image'/>
+        <div className='login-form'>
+            <h2>Anmelden</h2>
+            <form>
+                <label htmlFor='username'>Benutzername:</label>
+                <input type='text' id='username' name='username' required />
+                <label htmlFor='password'>Passwort:</label>
+                <input type='password' id='password' name='password' required />
+                <button type='submit'>Anmelden</button>
+            </form>
+            <p>Noch keinen Account? <a href='/register'>Registrieren</a></p>
+        </div>
+    </div>
   )
 }
 
--- LiveSlope-frontend/src/pages/register.jsx
+++ LiveSlope-frontend/src/pages/register.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../styles/login.css'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='container'>
+        <img src='/background.jpg' alt='background' className='background-image'/>
+        <div className='login-form'>
+            <h2>Registrieren</h2>
+            <form>
+                <label htmlFor='username'>Benutzername:</label>
+                <input type='text' id='username' name='username' required />
+                <label htmlFor='password'>Passwort:</label>
+                <input type='password' id='password' name='password' required />
+                <label htmlFor='confirm-password'>Passwort bestätigen:</label>
+                <input type='password' id='confirm-password' name='confirm-password' required />
+                <button type='submit'>Registrieren</button>
+            </form>
+            <p>Hast du schon einen Account? <a href='/login'>Anmelden</a></p>
+        </div>
+    </div>
   )
 }
 
