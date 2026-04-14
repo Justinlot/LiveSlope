e74550853953fb85f87b02a0d3362b0695764aff
Justin Lotwin <161705340+Justinlot@users.noreply.github.com>
2026-04-14 14:12:32 +0200

    add ski areas to map + start at user location

--- LiveSlope-frontend/src/components/map.jsx
+++ LiveSlope-frontend/src/components/map.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import '../styles/map.css'
+import getNearSkiAreas from '../functions/getNearSkiAreas';
 
 function MapView() {
 	/**
@@ -12,14 +13,37 @@ function MapView() {
 
 	const map = useRef(null);
 
+	const handleMapMove = useCallback(() => {
+		/**
+		 * Adds markers for ski areas within the current map bounds to the map
+		 */
+		if (!map.current) return;
+		map.current.eachLayer(layer => {
+			if (layer instanceof L.Marker) {
+				map.current.removeLayer(layer);
+			}
+		});
+		const nearSkiAreas = getNearSkiAreas(map.current.getBounds());
+		nearSkiAreas.features.forEach(feature => {
+			const { name, difficulty } = feature.properties;
+			const [lng, lat] = feature.geometry.coordinates;
+			L.marker([lat, lng]).addTo(map.current)
+				.bindPopup(`<b>${name}</b><br>Schwierigkeit: ${difficulty}`);
+		});
+	}, []);
+
+
 	useEffect(() => {
 		if (!map.current) {
-			map.current = L.map(mapRef.current).setView([51.505, -0.09], 13);
+			map.current = L.map(mapRef.current).setView([48, 9], 6);
+			map.current.locate({ setView: true, maxZoom: 16 });
 			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 			}).addTo(map.current);
+			
+			map.current.on('moveend', handleMapMove);
 		}
-	}, []);
+	}, [handleMapMove]);
 
 	return (
 		<div className='map-container' ref={mapRef}></div>
---
+++ LiveSlope-frontend/src/functions/getNearSkiAreas.js
@@ -0,0 +1,19 @@
+import skiAreas from "../assets/ski-areas-placeholder";
+
+export default function getNearSkiAreas(bounds) {
+    /**
+     * Placeholder function to return ski areas within the given map bounds
+     * This should be replaced with an actual API call to fetch ski areas based on the map bounds
+     */
+    let nearSkiAreas = []
+    skiAreas.features.forEach(feature => {
+        const [lng, lat] = feature.geometry.coordinates;
+        if(bounds && bounds.contains([lat, lng])) {
+            nearSkiAreas.push(feature);
+        }
+    });
+
+    console.log(nearSkiAreas);
+
+    return { type: "FeatureCollection", features: nearSkiAreas };
+}
