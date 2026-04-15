977c3b8b08f774b264506f03c76d1974242626be
Justin Lotwin <161705340+Justinlot@users.noreply.github.com>
2026-04-15 14:46:25 +0200

    make panels responsive

--- LiveSlope-frontend/src/components/footer.jsx
+++ LiveSlope-frontend/src/components/footer.jsx
@@ -1,9 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import favoriteIcon from '../img/favorite.svg'
 import logoutIcon from '../img/logout.svg'
 import profileIcon from '../img/profile.svg'
 import '../styles/headerAndFooter.css'
 import AuthContext from '../assets/auth-context'
+import getFavoriteSkiAreas from '../functions/getFavoriteSkiAreas'
+import { createPortal } from 'react-dom'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { changePasswordScheme } from '../zod-schemes/change-password-scheme'
+import SkiAreaCard from './ski-area-card'
+import Popup from './popup'
 
 function Footer() {
 
@@ -11,7 +18,33 @@ function Footer() {
 	 * Component displaying the footer with icons or login button (only shown on smaller screens)
 	 */
 
-	const { loggedIn, logout } = useContext(AuthContext);
+	const { register, handleSubmit, formState: { errors } } = useForm({resolver: zodResolver(changePasswordScheme)});
+
+	const { username, loggedIn, logout, changePassword } = useContext(AuthContext);
+
+	const [popupOpen, setPopupOpen] = useState(false);
+	
+	const [profilePanelOpen, setProfilePanelOpen] = useState(false);
+	const [favoritePanelOpen, setFavoritePanelOpen] = useState(false);
+
+	const favoriteSkiAreas = getFavoriteSkiAreas();
+
+
+	useEffect(() => {
+		if (!profilePanelOpen) return;
+	
+		const handleClickOutside = (event) => {
+		  if (!event.target.closest('.panel') && !event.target.closest('#profile-icon')) {
+			setProfilePanelOpen(false);
+		  }
+		};
+		document.addEventListener('click', handleClickOutside);
+		return () => {
+		  document.removeEventListener('click', handleClickOutside);
+		};
+	  }, [profilePanelOpen]);
+
+
 	return (
 		<div className='footer'>
 			{loggedIn ?
@@ -20,11 +53,14 @@ function Footer() {
 						src={favoriteIcon}
 						alt="Favorite Icon"
 						className="icon"
+						onClick={() => setFavoritePanelOpen(!favoritePanelOpen)}
 					/>
 					<img
 						src={profileIcon}
 						alt="Profile Icon"
+						id='profile-icon'
 						className="icon"
+						onClick={() => setProfilePanelOpen(!profilePanelOpen)}
 					/>
 					<img
 						src={logoutIcon}
@@ -37,6 +73,48 @@ function Footer() {
 				<>
 					<a href='/login' className='button'>Anmelden</a>
 				</>
+			}
+			{profilePanelOpen && createPortal(
+			<div className='panel'>
+				<h3>Profil</h3>
+				<p>Benutzername: {username}</p>
+				<button onClick={() => setPopupOpen(true)}>Passwort ändern</button>
+			</div>, document.body
+			)}
+	
+			{favoritePanelOpen && createPortal(
+			<div className='panel favorites'>
+				{favoriteSkiAreas?.length > 0 ?
+					<ul>
+						{favoriteSkiAreas.map((feature, index) => (
+							<SkiAreaCard skiArea={feature} index={index} key={index} />
+						))}
+					</ul>
+				: (<p>Kein Skigebiet in der Nähe gefunden.</p>)}
+				<button className='side-panel-close' onClick={() => setFavoritePanelOpen(false)}>Schließen</button>
+			</div>, document.body
+			)}
+			{popupOpen &&
+			<Popup onClose={() => setPopupOpen(false)}>
+				<div className='popup-profile'>
+				<h2>Passwort ändern</h2>
+				<form onSubmit={handleSubmit((data) => {
+					changePassword(data.oldPassword, data.password);
+					setPopupOpen(false);
+				})}>
+					<label htmlFor="oldPassword">Altes Passwort:</label>
+					<input type="password" id="oldPassword" {...register('oldPassword')} />
+					<span className='form-errors'>{errors.oldPassword?.message}</span>
+					<label htmlFor="newPassword">Neues Passwort:</label>
+					<input type="password" id="newPassword" {...register('password')} />
+					<span className='form-errors'>{errors.password?.message}</span>
+					<label htmlFor="confirmPassword">Neues Passwort bestätigen:</label>
+					<input type="password" id="confirmPassword" {...register('confirmPassword')} />
+					<span className='form-errors'>{errors.confirmPassword?.message}</span>
+					<button type="submit">Passwort ändern</button>
+				</form>
+				</div>
+			</Popup>
 			}
 		</div>
 	)
