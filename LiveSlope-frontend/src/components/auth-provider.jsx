ef11c787ab05635c29bf9c2bcc3fd6b841940bd7
Justinlot <161705340+Justinlot@users.noreply.github.com>
2026-04-22 14:20:34 +0200

    (fix) remove placeholder username

--- LiveSlope-frontend/src/components/auth-provider.jsx
+++ LiveSlope-frontend/src/components/auth-provider.jsx
@@ -11,7 +11,7 @@ export default function AuthProvider({ children }) {
 
     const navigate = useNavigate();
 
-    const [username, setUsername] = useState('justin');
+    const [username, setUsername] = useState(null);
 
     useEffect(() => {
         // On mount, check if user is already logged in
